19b0e26178c8d4faa558f544ff964150af731fd1
HoangNDM <hoangndm6@fpt.com>
2023-12-05 11:49:35 +0700

    fix get proposals

--- src/logic/safe/store/actions/fetchTransactionDetails.ts
+++ src/logic/safe/store/actions/fetchTransactionDetails.ts
@@ -17,57 +17,57 @@ const updateTransactionDetails = createAction<TransactionDetailsPayload>(UPDATE_
 
 export const fetchTransactionDetailsById =
   ({ transactionId, auraTxId }: { transactionId?: string; auraTxId?: string }) =>
-    async (dispatch: Dispatch, getState: () => AppReduxState): Promise<Transaction['txDetails']> => {
-      const transaction = getTransactionByAttribute(getState(), {
-        attributeValue: transactionId ? transactionId : auraTxId,
-        attributeName: transactionId ? 'id' : 'auraTxId',
-      })
-      const safeAddress = extractSafeAddress()
-      const chainId = currentChainId(getState())
-      const chainInfo = getChainInfo() as any
+  async (dispatch: Dispatch, getState: () => AppReduxState): Promise<Transaction['txDetails']> => {
+    const transaction = getTransactionByAttribute(getState(), {
+      attributeValue: transactionId ? transactionId : auraTxId,
+      attributeName: transactionId ? 'id' : 'auraTxId',
+    })
+    const safeAddress = extractSafeAddress()
+    const chainId = currentChainId(getState())
+    const chainInfo = getChainInfo() as any
 
-      if (transaction?.txDetails || !safeAddress) {
+    if (transaction?.txDetails || !safeAddress) {
+      return
+    }
+
+    try {
+      const { Data, ErrorCode } = await getTxDetailById(safeAddress, transactionId, auraTxId)
+      if (ErrorCode !== MESSAGES_CODE.SUCCESSFUL.ErrorCode) {
         return
       }
+      const extraDetails: any = {}
+      const listProposals = (await getProposals()).data?.[chainInfo.environment]?.proposal
+      const proposal = listProposals?.find((proposal) => proposal.proposal_id === Data?.Messages?.[0]?.proposalId)
 
-      try {
-        const { Data, ErrorCode } = await getTxDetailById(safeAddress, transactionId, auraTxId)
-        if (ErrorCode !== MESSAGES_CODE.SUCCESSFUL.ErrorCode) {
-          return
+      if (proposal) {
+        const proposalDetail = {
+          id: proposal.proposal_id,
+          title: proposal.title,
+          status: proposal.status,
+          votingEnd: proposal.voting_end_time,
         }
-        const extraDetails: any = {}
-        const listProposals = (await getProposals()).data?.[chainInfo.environment]?.proposal
-        const proposal = listProposals?.find((proposal) => proposal.proposal_id === Data?.Messages?.[0]?.proposalId)
+        extraDetails.proposalDetail = proposalDetail
+      }
 
-        if (proposal) {
-          const proposalDetail = {
-            id: proposal.proposal_id,
-            title: proposal.content[0].content.title,
-            status: proposal.status,
-            votingEnd: proposal.voting_end_time,
-          }
-          extraDetails.proposalDetail = proposalDetail
-        }
-
-        const transactionDetails: any = {
-          txId: Data?.MultisigTxId?.toString() || null,
-          auraTxId: Data?.AuraTxId?.toString() || null,
-          executedAt: Data.Timestamp
-            ? new Date(Data.Timestamp).getTime()
-            : Data.Executor
-              ? new Date(Data.Executor.updatedAt).getTime()
-              : null,
-          createAt: Data.CreatedAt ? new Date(Data.CreatedAt).getTime() : null,
-          txStatus: (Data.Status == '0' ? TransactionStatus.SUCCESS : Data.Status) as TransactionStatus,
-          txMessage: Data?.Messages?.length ? Data?.Messages : [],
-          rawMessage: Data?.RawMessages,
-          deletedBy: Data?.Deleter,
-          fee: Data?.Fee?.toString() || 0,
-          gas: Data?.Gas?.toString() || 0,
-          txHash: Data?.TxHash || null,
-          confirmationsRequired: Data.ConfirmationsRequired,
-          confirmations: Data?.Confirmations?.map(
-            (cf) =>
+      const transactionDetails: any = {
+        txId: Data?.MultisigTxId?.toString() || null,
+        auraTxId: Data?.AuraTxId?.toString() || null,
+        executedAt: Data.Timestamp
+          ? new Date(Data.Timestamp).getTime()
+          : Data.Executor
+          ? new Date(Data.Executor.updatedAt).getTime()
+          : null,
+        createAt: Data.CreatedAt ? new Date(Data.CreatedAt).getTime() : null,
+        txStatus: (Data.Status == '0' ? TransactionStatus.SUCCESS : Data.Status) as TransactionStatus,
+        txMessage: Data?.Messages?.length ? Data?.Messages : [],
+        rawMessage: Data?.RawMessages,
+        deletedBy: Data?.Deleter,
+        fee: Data?.Fee?.toString() || 0,
+        gas: Data?.Gas?.toString() || 0,
+        txHash: Data?.TxHash || null,
+        confirmationsRequired: Data.ConfirmationsRequired,
+        confirmations: Data?.Confirmations?.map(
+          (cf) =>
             ({
               signature: cf.signature,
               signer: {
@@ -77,31 +77,31 @@ export const fetchTransactionDetailsById =
               },
               submittedAt: new Date(cf.createdAt).getTime(),
             } as MultisigConfirmation),
-          ),
-          executor: !Data?.Executor
-            ? null
-            : {
+        ),
+        executor: !Data?.Executor
+          ? null
+          : {
               logoUri: null,
               name: null,
               value: Data.Executor.ownerAddress,
             },
-          rejectors: Data?.Rejectors?.map((re) => ({ logoUri: null, name: null, value: re.ownerAddress } as AddressEx)),
-          extraDetails,
-          autoClaimAmount: Data?.AutoClaimAmount,
-          sequence: Data?.Sequence,
-          logs: Data?.Logs,
-          txMemo: Data?.Memo,
-        }
-
-        dispatch(
-          updateTransactionDetails({
-            chainId,
-            transactionId: transactionDetails.txId,
-            safeAddress,
-            value: transactionDetails,
-          }),
-        )
-      } catch (error) {
-        console.error(`Failed to retrieve transaction details`, error.message)
+        rejectors: Data?.Rejectors?.map((re) => ({ logoUri: null, name: null, value: re.ownerAddress } as AddressEx)),
+        extraDetails,
+        autoClaimAmount: Data?.AutoClaimAmount,
+        sequence: Data?.Sequence,
+        logs: Data?.Logs,
+        txMemo: Data?.Memo,
       }
+
+      dispatch(
+        updateTransactionDetails({
+          chainId,
+          transactionId: transactionDetails.txId,
+          safeAddress,
+          value: transactionDetails,
+        }),
+      )
+    } catch (error) {
+      console.error(`Failed to retrieve transaction details`, error.message)
     }
+  }
