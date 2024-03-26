729aad996a3b0decefbaa7e09813870c5280012e
HoangNDM6 <mhoang.9699@gmail.com>
2024-03-26 17:36:09 +0700

    fix send ibc token

--- src/pages/Transactions/TxActionModal/Send/index.tsx
+++ src/pages/Transactions/TxActionModal/Send/index.tsx
@@ -33,7 +33,7 @@ export default function Execute({ open, onClose, data, sendTx, rejectTx, disable
   const [txMemo, setTxMemo] = useState(data?.txDetails?.txMemo)
 
   const isNativeToken = data?.txDetails?.txMessage[0]?.denom === coinConfig?.find((e) => e?.type === 'native')?.denom
-  const otherToken = coinConfig?.find((e) => e.denom === data?.txDetails?.txMessage[0]?.denom)
+  const otherToken = coinConfig?.find((e) => e?.denom ?? e?.cosmosDenom === data?.txDetails?.txMessage[0]?.denom)
 
   const totalAllocationAmount = isNativeToken
     ? formatNativeToken(
